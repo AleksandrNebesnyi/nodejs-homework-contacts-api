0f0727650d5cd052dd2ce7b9ebff1cef2d81997c
AleksandrNebesnyi <a.nebesnyi@gmail.com>
2022-03-18 15:45:11 +0200

    finish base task

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,43 @@
 const express = require('express');
 
 const router = express.Router();
+const {
+  getContacts,
+  getContactsById,
+  addContacts,
+  deleteContact,
+  patchContact,
+} = require('../../controllers/contactsController'); // Импорт контроллеров маршрутов
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message1' });
-});
+const {
+  addContactValidation,
+  updateContactValidation,
+} = require('../../middlewares/contactValidation');
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' });
-});
+router.get('/', getContacts); // Роут для списка всех контактов
+router.get('/:contactId', getContactsById); // Роут для контакта по id
+router.post('/', addContactValidation, addContacts); // Роут для создания контакта
+router.patch('/:contactId', updateContactValidation, patchContact); // Роут для обновления контакта
+router.delete('/:contactId', deleteContact); // Роут для удаления контакта
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' });
-});
+// router.get('/', async (req, res, next) => {
+//   res.json({ message: 'template message1' });
+// });
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' });
-});
+// router.get('/:contactId', async (req, res, next) => {
+//   res.json({ message: 'template message' });
+// });
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' });
-});
+// router.post('/', async (req, res, next) => {
+//   res.json({ message: 'template message' });
+// });
+
+// router.delete('/:contactId', async (req, res, next) => {
+//   res.json({ message: 'template message' });
+// });
+
+// router.put('/:contactId', async (req, res, next) => {
+//   res.json({ message: 'template message' });
+// });
 
 module.exports = router;
