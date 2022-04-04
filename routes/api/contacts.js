60e64f360a366262786249c4309d8ca54ac617ac
AleksandrNebesnyi <a.nebesnyi@gmail.com>
2022-04-04 10:11:48 +0300

    fin base task

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -9,7 +9,7 @@ const {
   updateContactStatus,
   deleteContact,
 } = require('../../controllers/contactsController'); // Импорт контроллеров маршрутов
-
+const authenticate = require('../../middlewares/authenticate');
 const {
   addContactValidation,
   updateContactValidation,
@@ -17,19 +17,21 @@ const {
 } = require('../../middlewares/contactValidation');
 const { asyncWrapper } = require('../../helpers/asyncWrapper'); // Мидлвар универсального обработчика try catch
 
-router.get('/', asyncWrapper(getContacts)); // Роут для списка всех контактов
-router.get('/:contactId', asyncWrapper(getContactsById)); // Роут для контакта по id
-router.post('/', addContactValidation, asyncWrapper(addContacts)); // Роут для создания контакта
+router.get('/', authenticate, asyncWrapper(getContacts)); // Роут для списка всех контактов
+router.get('/:contactId', authenticate, asyncWrapper(getContactsById)); // Роут для контакта по id
+router.post('/', authenticate, addContactValidation, asyncWrapper(addContacts)); // Роут для создания контакта
 router.patch(
   '/:contactId',
+  authenticate,
   updateContactValidation,
   asyncWrapper(updateContact),
 ); // Роут для обновления контакта
 router.patch(
-  '/contactId/favorite',
+  '/:contactId/favorite',
+  authenticate,
   updateContactStatusValidation,
   asyncWrapper(updateContactStatus),
 ); //  Роут статуса контакта
-router.delete('/:contactId', asyncWrapper(deleteContact)); // Роут для удаления контакта
+router.delete('/:contactId', authenticate, asyncWrapper(deleteContact)); // Роут для удаления контакта
 
 module.exports = router;
